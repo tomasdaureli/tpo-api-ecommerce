ad58b5cfff3b1ecaa4d08bb04b4343de8a2e0654
tomasdaureli <tomasdaureli@gmail.com>
2024-04-25 22:47:15 -0300

    add catalog route

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,21 @@
 import { Routes, Route, Link } from 'react-router-dom'
 import './App.css'
 import { Home } from './components/Home'
-import { ProductList } from './components/ProductList'
-import { Header } from './components/Header'
-import { useState } from 'react'
+import { Catalog } from './components/Catalog'
 
 function App() {
 
-  const [allProducts, setAllProducts] = useState([])
-  const [total, setTotal] = useState(0)
-  const [countProducts, setCountProducts] = useState(0)
-
   return (
     <>
-      <Header 
-        allProducts={allProducts}
-        setAllProducts={setAllProducts}
-        total={total}
-        setTotal={setTotal}
-        countProducts={countProducts}
-        setCountProducts={setCountProducts} />
-      <ProductList 
-        allProducts={allProducts}
-        setAllProducts={setAllProducts}
-        total={total}
-        setTotal={setTotal}
-        countProducts={countProducts}
-        setCountProducts={setCountProducts} />
       <ul>
         <li><Link to="/home">Home</Link></li>
+        <li><Link to="/catalogo">Catalogo</Link></li>
       </ul>
       <div>
         <Routes>
           <Route path='/' />
           <Route path='/home' element={<Home />} />
+          <Route path='/catalogo' element={<Catalog />} />
         </Routes>
       </div>
     </>
