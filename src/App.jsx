9294dcb00440c1264f3c3d392fb253710fd7dc82
tomasdaureli <tomasdaureli@gmail.com>
2024-04-25 22:17:06 -0300

    practice

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,32 @@
 import { Routes, Route, Link } from 'react-router-dom'
 import './App.css'
-import { Home } from './Home'
+import { Home } from './components/Home'
+import { ProductList } from './components/ProductList'
+import { Header } from './components/Header'
+import { useState } from 'react'
 
 function App() {
+
+  const [allProducts, setAllProducts] = useState([])
+  const [total, setTotal] = useState(0)
+  const [countProducts, setCountProducts] = useState(0)
+
   return (
     <>
-      <h1>Hello world</h1>
-      <h2>Start project</h2>
+      <Header 
+        allProducts={allProducts}
+        setAllProducts={setAllProducts}
+        total={total}
+        setTotal={setTotal}
+        countProducts={countProducts}
+        setCountProducts={setCountProducts} />
+      <ProductList 
+        allProducts={allProducts}
+        setAllProducts={setAllProducts}
+        total={total}
+        setTotal={setTotal}
+        countProducts={countProducts}
+        setCountProducts={setCountProducts} />
       <ul>
         <li><Link to="/home">Home</Link></li>
       </ul>
