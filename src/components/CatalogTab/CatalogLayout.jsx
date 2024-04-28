ac51cab18a245cebc7d62087d5f5668298b19c4e
tomasdaureli <tomasdaureli@gmail.com>
2024-04-28 20:59:41 -0300

    catalog layout change

--- src/components/CatalogTab/CatalogLayout.jsx
+++ src/components/CatalogTab/CatalogLayout.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 import { Outlet } from 'react-router-dom';
 
-export function CatalogLayout({ children }) {
+export function CatalogLayout() {
   return (
     <Outlet />
   )
