68291c1f915702789363da14e8dceb9478254664
tomasdaureli <tomasdaureli@gmail.com>
2024-04-30 13:28:34 -0300

    add checkout buy

---
+++ src/components/BuyTab/Buy.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import { useNavigate } from 'react-router-dom'
+
+export default function Buy() {
+
+    const navigate = useNavigate();
+
+    const onBackHomePage = () => {
+        navigate('/');
+    }
+
+  return (
+    <>
+        <h2>Gracias por su compra</h2>
+        <button onClick={onBackHomePage}>Volver a la pagina de inicio</button>
+    </>
+  )
+}
